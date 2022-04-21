41c2450988e1d3d083cd84ae559e0091d8367619
Akarsh Jaiswal <akarsh.jais@gmail.com>
2022-04-21 21:52:24 +0530

    Day 3 Programs

--- Main.java
+++ Main.java
@@ -1,5 +1,104 @@
 package Day3;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+	public static void main(String[] args) {
+
+//		//Fibonacci  Series -> nth term
+//		Scanner sc = new Scanner(System.in);
+//		int n =sc.nextInt();
+//		int a = 0;
+//		int b = 1;
+//		
+//		if(n==1) {
+//			System.out.println(a);
+//		}
+//		else if(n==2) {
+//			System.out.println(b);
+//		}
+//		else {
+//			int c = 1;
+//			for(int i=0;i<n-2;i++) {
+//				c = a+b;
+//				a = b;
+//				b = c;
+//			}
+//			System.out.println(c);
+//			
+//		}
+		
+		
+//		//ascii values
+//		char ch = 'O';
+//		int ascii = ch;
+//		System.out.println(ascii);
+//		if(ascii>=65 && ascii<=90) {
+//			System.out.println("upper case");
+//		}
+//		else {
+//			System.out.println("lower case");
+//		}
+		
+//		int num = 4567;
+//		while(num>0) {
+//			int rem = num%10;
+//			System.out.println(rem);
+//			
+//			num = num/10;
+//		}
+
+//		int a = 378/10;
+//		System.out.println(a);
+		
+//		//Print 1->5
+//		
+//		for(int i=1;i<6;i++) {
+//			System.out.println(i);
+//		}
+//		
+//		//Program to print 5->1
+//		for(int i=5;i>=1;i--) {
+//			System.out.println(i);
+//		}
+//		
+//		//Print like 1 4 7 10 13 16
+//		for(int i=1;i<17;i=i+3) {
+//			System.out.println(i);
+//		}
+//		
+//		//Sum all numbers from 1 to 9
+//		int sum = 0;
+//		for(int i=1;i<9;i++) {
+//			sum = sum+i;
+//		}
+//		System.out.println(sum);
+//		
+//		//Table of 7
+//		for(int i=1;i<=10;i++) {
+//			System.out.println(i*7);
+//		}
+		
+//		int i = 1;
+//		while(i<=10) {
+//			System.out.println(i*7);
+//			i++;
+//		}
+	
+
+//		Scanner sc = new Scanner(System.in);
+//		String ch = sc.next();
+//		int ascii = ch.charAt(0);
+//		System.out.println(ascii);
+
+		
+//		char ch1 = 'A';
+//		int ascii_val = ch1;
+//		System.out.println(ascii_val);
+		
+		
+//		Scanner sc = new Scanner(System.in);
+//		String name = sc.next();
+//		System.out.println(name);
+	}
 }
